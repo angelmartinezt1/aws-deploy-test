ec470909abc916248ea3ba0506a514f3ad7e5ee6
angel.martinez <angel.martinez@claroshop.com>
2025-05-04 01:39:51 -0600

    Cambios desde local

--- src/express.js
+++ src/express.js
@@ -20,7 +20,7 @@ app.get('/api/health', (req, res) => {
 
 // Health check endpoint
 app.get('/github', (req, res) => {
-  formatResponse(res, 200, true, { status: 'OK' }, 'THola Nati')
+  formatResponse(res, 200, true, { status: 'OK' }, 'Desde VSCode')
 })
 
 app.use(notFoundMiddleware)
